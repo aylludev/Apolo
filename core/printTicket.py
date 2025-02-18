5e995dd8481843f999a69b2879f69ecac5831f80
aylludev <cafo1995@hotmail.com>
2025-02-17 20:22:52 -0500

    fluid containers

--- core/printTicket.py
+++ core/printTicket.py
@@ -1,6 +1,6 @@
 from config.wsgi import *
 from django.template.loader import get_template
-from weasyprint import HTML, CSS
+#from weasyprint import HTML, CSS
 from config import settings
 
 def printTicket():
@@ -8,7 +8,7 @@ def printTicket():
     context = {"name": "William Jair Dávila Vargas"}
     html_template = template.render(context)
     css_url = os.path.join(settings.BASE_DIR, 'static/lib/bootstrap-4.4.1-dist/css/bootstrap.min.css')
-    HTML(string=html_template).write_pdf(target="ticket.pdf", stylesheets=[CSS(css_url)])
+    #HTML(string=html_template).write_pdf(target="ticket.pdf", stylesheets=[CSS(css_url)])
 
 
 printTicket()
